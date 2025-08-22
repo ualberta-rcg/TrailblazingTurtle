bdc871c1c6c8087a163078225bb0d7d56f459db1
Karim Ali <kali2@ualberta.ca>
2025-08-22 16:07:03 +0000

    logging

--- jobstats/views.py
+++ jobstats/views.py
@@ -573,6 +573,7 @@ def graph_cpu(request, username, job_id):
     context = context_job_info(request, username, job_id)
 
     query = 'rate(slurm_job_core_usage_total{{slurmjobid=~"{}", {}}}[{}s]) / 1000000000'.format(context['id_regex'], prom.get_filter(), prom.rate('slurm-job-exporter'))
+    print(query)
     stats = prom.query_prometheus_multiple(
         query,
         context['start'],
@@ -1777,6 +1778,7 @@ def graph_cache_rate(request, username, job_id, l_cache):
         instances=instances,
         filter=prom.get_filter(),
         rate=prom.rate('pcm-sensor-server'))
+    print(query_cache)
     stats_cache = prom.query_prometheus_multiple(
         query_cache,
         context['start'],
