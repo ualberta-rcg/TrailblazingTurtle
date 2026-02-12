e5ce3bfb1c8a0cc56d6441cc3ed3bc1defb200b4
Karim Ali <kali2@ualberta.ca>
2026-02-12 10:33:10 -0700

    config

--- userportal/settings/10-base.py
+++ userportal/settings/10-base.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'amii_portal!'
+SECRET_KEY = 'changeme'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
